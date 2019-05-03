2d06767430306ad6cefe19231fe73a9eeff5bcda
Thomas-Tuttle <48684599+Thomas-Tuttle@users.noreply.github.com>
2019-05-03 11:56:25 -0400

    Top Bar constructed and styled

--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,24 +1,110 @@
 import React from 'react';
-
+import styled from "styled-components";
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
+
 const TopBar = () => {
   return (
-    <div className="top-bar">
-      <div className="container">
-        <div className="container-left">
-          <span>TOPICS</span><span>SEARCH</span>
-        </div>
-        <div className="container-center">
-          <span>GENERAL</span><span>BROWNBAG</span><span>RANDOM</span><span>MUSIC</span><span>ANNOUNCEMENTS</span>
-        </div>
-        <div className="container-right">
-          <span>LOG IN</span>
-        </div>
-      </div>
-    </div>
+    <Container>
+      <Header>
+        <LeftNavContainer>
+          <LeftLink>TOPICS</LeftLink>
+          <LeftLink>SEARCH</LeftLink>
+        </LeftNavContainer>
+        <CenterNavContainer>
+          <CenterLink>GENERAL</CenterLink>
+          <CenterLink>BROWNBAG</CenterLink>
+          <CenterLink>RANDOM</CenterLink>
+          <CenterLink>MUSIC</CenterLink>
+          <CenterLink>ANNOUNCEMENTS</CenterLink>
+        </CenterNavContainer>
+        <LogInContainer>
+          <LogIn>LOG IN</LogIn>
+        </LogInContainer>
+      </Header>
+    </Container>
   )
 }
 
 export default TopBar;
+
+
+const Container = styled.div`
+  width: 100%;
+  height: 45px;
+  position: fixed;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  background-color: #332f2f;
+`;
+
+const Header = styled.div`
+  width: 100%;
+  padding: 0 10px;
+  display: flex;
+  flex-direction: row;
+  color: white;
+  letter-spacing: 1.25px;
+  
+  @media (min-width: 1200px) {
+    width: 80%;
+  }
+`;
+
+const LeftNavContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex: 1;
+  align-items: center;
+`;
+
+const LeftLink = styled.h1`
+  cursor: pointer;
+  margin-right: 25%;
+  font-weight: bold;
+  font-size: 12px;
+
+  &:hover {
+    color: red;
+  }
+`;
+
+const CenterNavContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex: 3;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CenterLink = styled.h1`
+  cursor: pointer;
+  margin: 2.5%;
+  font-size: 8px;
+
+  &:hover {
+    text-decoration: underline;
+    color: red;
+  }
+`;
+
+const LogInContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex: 1;
+  justify-content: flex-end;
+  align-items: center;
+  
+`;
+
+const LogIn = styled.h1`
+  cursor: pointer;
+  font-size: 12px;
+  font-weight: bold;
+
+  &:hover {
+    color: red;
+  }
+`;
